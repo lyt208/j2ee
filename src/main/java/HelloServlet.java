e28c7425ad603bc6e264983e15580d73f8eee34a
lyt208 <lyt208208>
2017-12-19 17:21:00 +0800

    test0

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -4,13 +4,13 @@ import java.io.IOException;
 import java.util.Date;
 
 public class HelloServlet extends javax.servlet.http.HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)  {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
         try {
             response.getWriter().println("<h1>Hello Servlet!</h1>");
             response.getWriter().println(new Date().toLocaleString());
             response.getHeaderNames();
             response.getStatus();
-response.getHeader("aa");
+            response.getHeader("aa");
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
